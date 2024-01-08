3dd32c2ce31d498fc0fd428e085382bebddd84b1
Shubham Kumar <shubhjack@outlook.com>
2024-01-08 18:28:14 +0530

    v1.0.3 code changes

--- ExcelXporter.cs
+++ ExcelXporter.cs
@@ -84,5 +84,83 @@ namespace ExcelXporter
                 FileDownloadName =  $"{fileDownloadName}.xlsx"
             };
         }
+
+        public static FileContentResult ExportToExcelMultipleSheets(this List<dynamic> sheetData, string filename)
+        {
+            Stream stream = new MemoryStream();
+            using (var spreadsheetDocument = SpreadsheetDocument.Create(stream, SpreadsheetDocumentType.Workbook))
+            {
+                var workbookPart = spreadsheetDocument.AddWorkbookPart();
+                workbookPart.Workbook = new Workbook();
+                var sheets = spreadsheetDocument.WorkbookPart.Workbook.AppendChild(new Sheets());
+
+                int sheetId = 0;
+                foreach (var objList in sheetData)
+                {
+                    sheetId++;
+                    var worksheetPart = workbookPart.AddNewPart<WorksheetPart>();
+                    worksheetPart.Worksheet = new Worksheet(new SheetData());
+
+                    var sheet = new Sheet()
+                    {
+                        Id = spreadsheetDocument.WorkbookPart.GetIdOfPart(worksheetPart),
+                        SheetId = (uint)sheetId,
+                        Name = $"Sheet {sheetId}"
+                    };
+                    sheets.Append(sheet);
+                    List<object> newObjList = new();
+                    newObjList.AddRange(objList);
+                    var columnNames = newObjList[0].ToDictionary().Keys;
+
+                    var headerRow = new Row();
+                    foreach (var name in columnNames)
+                    {
+                        headerRow.Append(
+                            new Cell() { CellValue = new CellValue(name), DataType = CellValues.String }
+                        );
+                    }
+
+                    worksheetPart.Worksheet.GetFirstChild<SheetData>().AppendChild(headerRow);
+                    foreach (var obj in newObjList)
+                    {
+
+                        var values = obj.ToDictionary().Values;
+                        var dataRow = new Row();
+                        foreach (var value in values)
+                        {
+                            if (int.TryParse(value, out int result))
+                            {
+                                dataRow.Append(
+                                    new Cell() { CellValue = new CellValue(result.ToString()), DataType = CellValues.Number }
+                                );
+                            }
+                            else
+                            {
+                                dataRow.Append(
+                                    new Cell() { CellValue = new CellValue(value), DataType = CellValues.String }
+                                );
+                            }
+                        }
+                        worksheetPart.Worksheet.GetFirstChild<SheetData>().AppendChild(dataRow);
+                    }
+                }
+                workbookPart.Workbook.Save();
+            }
+
+            stream.Position = 0;
+            byte[] bytes;
+            using (var memoryStream = new MemoryStream())
+            {
+                stream.CopyTo(memoryStream);
+                bytes = memoryStream.ToArray();
+            }
+
+            var fileDownloadName = filename.Replace(".xls", string.Empty).Replace(".xlsx", string.Empty).Trim();
+            return new FileContentResult(bytes, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+            {
+                FileDownloadName = $"{fileDownloadName}.xlsx"
+            };
+        }
+
     }
 }
--- README.md
+++ README.md
@@ -2,7 +2,72 @@
 
 Just create any model list and pass this to the library and it will generate the excel for you in no time.
 
-Sample call:
-return ExcelXporter.ExcelXporter.ExportToExcel(objList, filename);
+*--------------------------------------------------------*
+Sample call if you have a single data list to export:
+
+add namespace on top using ExcelXporter and use below -
+return objList.ExportToExcel("filename");
+*--------------------------------------------------------*
+
+*--------------------------------------------------------*
+Sample call if you have a multiple data list to export:
+
+add namespace on top using ExcelXporter and use below -
+
+        [HttpGet]
+        [Route("[action]")]
+        public IActionResult TestExportexcel()
+        {
+            List<TestModel> objList = new()
+            {
+                new TestModel ()
+                {
+                    Id = 1,
+                    Name = "John",
+                    Email = "john.doe@gmail.com"
+                },
+                new TestModel
+                {
+                    Id = 2,
+                    Name = "Wick",
+                    Email = "john.wick@gmail.com"
+                },
+            };
+
+            List<TestModel2> objList2 = new()
+            {
+                new TestModel2 ()
+                {
+                    Id = 1,
+                    Name = "John",
+                    Email = "john.doe@gmail.com",
+                    Address = "addr1"
+                },
+                new TestModel2
+                {
+                    Id = 2,
+                    Name = "Wick",
+                    Email = "john.wick@gmail.com",
+                    Address = "addr2"
+                },
+                new TestModel2
+                {
+                    Id = 3,
+                    Name = "Constantine",
+                    Email = "c.wick@gmail.com",
+                    Address = "addr3"
+                },
+            };
+            // This is the important part. You need to create a dynamic list object and add your list of different classes in it.
+            List<dynamic> listObj = new()
+            {
+                objList,
+                objList2
+            };
+            //now call the below extension method on the list created
+            return listObj.ExportToExcelMultipleSheets("filename"); 
+        }
+Above return statement will export multiple data list and will create separate sheet for each list
+*--------------------------------------------------------*
 
 Above code will download a excel.
